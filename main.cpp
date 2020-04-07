2727b6ce6310ad019d264c0f276b40472bd994c4
Josh Dorsey <dorseyja98@gmail.com>
2020-04-07 08:35:22 -0600

    Removed some test code for SourceFile, other minor changes

--- main.cpp
+++ main.cpp
@@ -14,36 +14,7 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    /*
-    SourceFile file("./sources/source.txt");
-
-    string parse = "";
-    // Read 20 characters from stream
-    int i = 20;
-    while (i) {
-        auto pos = file.position();
-        char got = file.get();
-        parse += got;
-        cout << "\'" << got << "\' (" << pos.line << ":" << pos.column << ")\n";
-        i--;
-    }
-
-    // Put some back
-    cout << "putting " << (parse.substr(10).size()) << " characters back.\n";
-    file.put_back(parse.substr(10));
-
-    // Read again
-    i = 20;
-    while (i) {
-        char got = file.get();
-        parse += got;
-        auto pos = file.position();
-        cout << "\'" << got << "\' (" << pos.line << ":" << pos.column << ")\n";
-        i--;
-    }
-
-    return 0;*/
-
+    // We need 3 arguments + the program name
     if (argc != 4) exit(1);
 
     // organizing filepaths
@@ -51,9 +22,9 @@ int main(int argc, char* argv[]) {
     string sourceFile = argv[2];
     string outputFile = argv[3];
 
-    Scanner scan(scannerFile);
-    scan.parseFile(sourceFile);
-    scan.outputParseData(outputFile);
+    Scanner scanner(scannerFile);
+    scanner.parseFile(sourceFile);
+    scanner.outputParseData(outputFile);
 
     return 0;
 }
